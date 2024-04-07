5002202f50e91bc4cc39fcf505580d7e43500a85
Jose Martinez <44812411+josemartinez111@users.noreply.github.com>
2024-04-06 21:50:01 -0400

    ⚡pushing -> `pushing changes: - Using sqlite as the first db for the microservices - Added prisma to the project as the orm - ran the first migration - added nestjs logging - added custom env environment

--- 02-Products-App/products-mcs/src/main.ts
+++ 02-Products-App/products-mcs/src/main.ts
@@ -1,8 +1,53 @@
+// FILE: main.ts
+// _______________________________________________
+
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { envs } from "src/config/envs";
+import { logWithColor, spacer } from 'src/utilities/coloredOutput';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+// _______________________________________________
+
+async function bootstrap(): Promise<void> {
+	// adding nestjs Logger
+	const logger = new Logger('BOOTSTRAP<main.ts>');
+	// Get the port from the environment variables
+	const port = envs.port;
+	
+	// Create the Nest application instance
+	const app = await NestFactory.create(AppModule);
+	// Start the application on port 3000
+	await app.listen(port);
+	
+	// Enable validation globally of all incoming requests
+	app.useGlobalPipes(
+		// Validate incoming data with the class-validator package
+		new ValidationPipe({
+			// Automatically transform payloads to instances of the DTO classes
+			whitelist: true,
+			// Forbid non-allow listed properties in the DTO classes
+			forbidNonWhitelisted: true,
+		}),
+	);
+	
+	// Check that the port number is type `number`
+	const isNumberPort = `\n\n[ IS_THE_PORT_TYPE_OF_NUMBER ${ port }? ]->: ${ !!(typeof port) }\n\n`;
+	// `!!(typeof port)` is the same as `typeof port ? true : false`
+	logWithColor(isNumberPort, 'dodgerBlue', true);
+	
+	// Log the host and port the server is running on
+	const runningOnPort = `Server is running on http://localhost:${ port }`;
+	const result = spacer(runningOnPort, 50);
+	logger.log(result);
 }
-bootstrap();
+
+// _______________________________________________
+
+bootstrap().catch((err: unknown): void => {
+	if (err instanceof Error)
+		console.error('Error starting the server:', err.message);
+	// Re-throw the error for further handling
+	throw err;
+});
+// _______________________________________________
---
+++ 02-Products-App/products-mcs/src/products/entities/product.entity.ts
@@ -0,0 +1,10 @@
+// FILE: products/entities/product.entity.ts
+// _______________________________________________
+// _______________________________________________
+
+export class Product {
+	id: number;
+	name: string;
+	price: number;
+}
+// _______________________________________________
